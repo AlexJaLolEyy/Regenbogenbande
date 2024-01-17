07068eda621f44d11cd4fb5c74c514f105163519
Alex <alexanderbenski@gmail.com>
2024-01-18 00:22:20 +0100

    ✨🎨 - create folder structure + default component for picture delete and edit

---
+++ lib/components/pictures/picture-delete/picture-delete.tsx
@@ -0,0 +1,12 @@
+import { Picture } from "../../../types/types"
+
+export default function PictureDelete({ picture }: { picture: Picture }
+
+) {
+    return (
+        <div>
+            <p>You are currently at Picture-Delete!</p>
+
+        </div>
+    )
+}
---
+++ lib/components/pictures/picture-edit/picture-edit.tsx
@@ -0,0 +1,11 @@
+import { Picture } from "../../../types/types"
+
+export default function PictureEdit({ picture }: { picture: Picture }
+
+) {
+    return (
+        <div>
+            <p>You are currently at Picture-Edit!</p>
+        </div>
+    )
+}
