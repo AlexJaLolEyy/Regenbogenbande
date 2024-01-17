99261860cde0c7d7c4f00e0eaf26e290bba9f72f
Alex <alexanderbenski@gmail.com>
2024-01-18 00:21:37 +0100

    ✨🎨 - create folder structure + component for navigation / navbar

---
+++ lib/components/Navigation/navigation.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation"
+import "./navigation.css"
+
+// maybe add currentPath as variable
+export default function Navigation({ }: {}
+) {
+
+    const pathname = usePathname();
+    const params = useParams();
+    const currentPath = pathname.split("/").length >= 3 ? pathname.split("/")[2] : ""
+    return (
+        <div>
+
+            {/* TODO replace with navbar component */}
+            <div className="navbar">
+                <Link href="/home">Home</Link>
+                <Link href="/videos">Videos</Link>
+                <Link href="/pictures">Pictures</Link>
+                <Link href="/quotes">Quote</Link>
+            </div>
+
+            {/* div for printing the current path (temporary) */}
+            <div>
+                <label>Pathname</label>
+                <p>{pathname}</p>
+
+                <label>Params</label>
+                <p>{params.toString()}</p>
+
+                <label>currentPath</label>
+                <p>{currentPath}</p>
+            </div>
+
+        </div>
+    )
+}
