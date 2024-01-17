a547bc8626a90f844c72fc89b93615c2a141ebc1
Alex <alexanderbenski@gmail.com>
2024-01-18 00:22:30 +0100

    ✨🎨 - create folder structure + default component for quote delete and edit

---
+++ lib/components/quotes/quote-delete/quote-delete.tsx
@@ -0,0 +1,12 @@
+import { Quote } from "../../../types/types"
+
+export default function QuoteDelete({ quote }: { quote: Quote }
+
+) {
+    return (
+        <div>
+            <p>You are currently at Quote-Delete!</p>
+
+        </div>
+    )
+}
---
+++ lib/components/quotes/quote-edit/quote-edit.tsx
@@ -0,0 +1,12 @@
+import { Quote } from "../../../types/types"
+
+export default function QuoteEdit({ quote }: { quote: Quote }
+
+) {
+    return (
+        <div>
+            <p>You are currently at Quote-Edit!</p>
+
+        </div>
+    )
+}
