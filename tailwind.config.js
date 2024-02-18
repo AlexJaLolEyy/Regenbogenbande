5e23b553ae8fcd696c609549bdaebec22a89d84a
Alex <alexanderbenski@gmail.com>
2024-02-18 23:52:43 +0100

    🔧 add config for tailwind and postcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import {nextui} from "@nextui-org/react";
+/** @type {import('tailwindcss').Config} */
+
+const config = {
+  content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
+
+export default config;
