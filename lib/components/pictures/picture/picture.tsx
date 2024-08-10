15c1e8ad4d5cb58ad439b87d158cb3fb3f283afc
alexanderbenski <alexanderbenski@gmail.com>
2024-08-10 00:58:49 +0000

    ✨🚧 adjusted picture component

--- lib/components/pictures/picture/picture.tsx
+++ lib/components/pictures/picture/picture.tsx
@@ -3,20 +3,29 @@ import type { Picture } from "../../../types/types"
 export default function PictureComponent({ picture }: { picture: Picture }
 
 ) {
-    console.log("final: ", picture)
+    console.log("final: ", picture.img);
+
+    var reader = new FileReader();
+
+    console.log("whole picture: ", picture)
+    
+    //var convert = URL.createObjectURL(picture.img);
+    //console.log("convert: ",convert);
+    
 
 
     return (
         <div>
             <p>-s-s-s-s-s-s-s-</p>
             <div className="pictureImg">
-                <img src={picture.img === null ? "Fehler" : URL.createObjectURL(picture.img as File)} alt="error while loading picture" width={"200"} height={"200"}></img>
+                <p>path: {URL.createObjectURL(picture.img)}</p>
+                <img src={picture.img === null ? "Fehler" : URL.createObjectURL(picture.img)} alt="error while loading picture" width={"200"} height={"200"}></img>
             </div>
             <div className="pictureTitle">
                 <p>{picture.title}</p>
             </div>
             <div className="pictureMetaData">
-                <p>{picture.createdAt.toISOString()}</p>
+                <p>{picture.createdAt.toString()}</p>
                 <p>{picture.uploadedBy.username}</p>
             </div>
 
