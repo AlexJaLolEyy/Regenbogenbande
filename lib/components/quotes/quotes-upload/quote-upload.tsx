8a233ec01367d111ce42e2b4becf4744f8c871a3
alexanderbenski <alexanderbenski@gmail.com>
2024-08-11 22:09:53 +0000

    ✨ adds upload component for quotes

---
+++ lib/components/quotes/quotes-upload/quote-upload.tsx
@@ -0,0 +1,15 @@
+import { Quote } from "../../../types/types"
+
+export default function QuoteUpload({ quote }: { quote: Quote }
+
+) {
+    return (
+        <div>
+            <p>You are currently at Quote-Upload!</p>
+
+            <p> This part needs a lot more work than every other upload combined.</p>
+            <p> The Developer is in charge of creating a unique option to upload a quote, bc u cant just select them like vids.</p>
+            <p> I dont wanna be that Dev... poor developer...</p>
+        </div>
+    )
+}
