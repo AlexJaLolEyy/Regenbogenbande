3bca13532ef65216e7e25175c4a0e706bf854b85
alexanderbenski <alexanderbenski@gmail.com>
2024-08-11 22:16:16 +0000

    ➕🔧 changed config to import .mp4 files as fakedata

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.(mp4|webm|ogg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            publicPath: `/_next/static/videos/`,
+            outputPath: `static/videos/`,
+            name: '[name].[hash].[ext]',
+          },
+        },
+      });
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
