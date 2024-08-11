a34adaabff6afb175652a0ff54cb5abd99d39dd5
alexanderbenski <alexanderbenski@gmail.com>
2024-08-11 22:08:10 +0000

    ✨ improves component for pictures

--- lib/components/pictures/picture/picture.tsx
+++ lib/components/pictures/picture/picture.tsx
@@ -1,34 +1,29 @@
-import type { Picture } from "../../../types/types"
+import type { Picture } from "../../../types/types";
 
-export default function PictureComponent({ picture }: { picture: Picture }
+export default function PictureComponent({ picture }: { picture: Picture }) {
+  
+  // TODO: replace html with card component + styling
 
-) {
-    console.log("final: ", picture.img);
-
-    var reader = new FileReader();
-
-    console.log("whole picture: ", picture)
-    
-    //var convert = URL.createObjectURL(picture.img);
-    //console.log("convert: ",convert);
-    
-
-
-    return (
-        <div>
-            <p>-s-s-s-s-s-s-s-</p>
-            <div className="pictureImg">
-                <p>path: {URL.createObjectURL(picture.img)}</p>
-                <img src={picture.img === null ? "Fehler" : URL.createObjectURL(picture.img)} alt="error while loading picture" width={"200"} height={"200"}></img>
-            </div>
-            <div className="pictureTitle">
-                <p>{picture.title}</p>
-            </div>
-            <div className="pictureMetaData">
-                <p>{picture.createdAt.toString()}</p>
-                <p>{picture.uploadedBy.username}</p>
-            </div>
-
-        </div>
-    )
+  return (
+    <div>
+      <hr></hr>
+      <div className="pictureImg">
+        <p>path: {URL.createObjectURL(picture.img)}</p>
+        <img
+          src={picture.img === null ? "Fehler" : URL.createObjectURL(picture.img)}
+          alt="error while loading picture"
+          width={"250"}
+          height={"250"}
+        ></img>
+      </div>
+      <div className="pictureTitle">
+        <p>Picture Title: {picture.title}</p>
+      </div>
+      <div className="pictureMetaData">
+        <p>Picture CreatedAt: {picture.createdAt.toString()}</p>
+        <p>Picture UploadedBy: {picture.uploadedBy.username}</p>
+      </div>
+      <hr></hr>
+    </div>
+  );
 }
