1851a2960208569c32d4c6fe57ba6651df5e2f1c
alexanderbenski <alexanderbenski@gmail.com>
2024-08-12 20:58:50 +0000

    💄 adds provider component into app root to feature the UI-Library

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import {Providers} from "./providers";
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    
+    <html lang="en" className='dark'>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
