643d6c93f489c5ed5020cf0d200f6d60a608d43c
alexanderbenski <alexanderbenski@gmail.com>
2024-08-14 21:45:42 +0000

    💄🚧 adds first version of picture display

--- lib/components/pictures/picture/picture.tsx
+++ lib/components/pictures/picture/picture.tsx
@@ -1,28 +1,74 @@
 import type { Picture } from "../../../types/types";
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import { Card, CardHeader, CardBody, Image, CardFooter, Button } from "@nextui-org/react";
 import "./picture.css";
 
 export default function PictureComponent({ picture }: { picture: Picture }) {
 
   // TODO: replace html with card component + styling
+  // TODO: find suitable picture size for display (width + height relation) z.b. 1980x1020...
+  // TODO: add "isPressable" to card (might need to redo styling)
 
   return (
     <div className="picture">
 
       <Card className="py-4">
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <p className="text-tiny uppercase font-bold">Daily Mix</p>
+          {/* <p className="text-tiny uppercase font-bold">Daily Mix</p> */}
           {/* <small className="text-default-500">12 Tracks</small> */}
-          <h4 className="font-bold text-large">Frontend Radio</h4>
+          <h4 className="font-bold text-large">{picture.title}</h4>
         </CardHeader>
         <CardBody className="overflow-visible py-2">
           <Image
             alt="Card background"
-            className="object-cover rounded-xl"
+            className="object-cover"
+            src={picture.img === null ? "Fehler" : URL.createObjectURL(picture.img)}
+            width={270}
+            sizes="600px"
+          />
+        </CardBody>
+        <CardFooter className="cardFooter">
+          <div className="flex gap-2 items-center">
+            <Image
+              alt="User Profile"
+              className="object-cover none"
+              src={picture.img === null ? "Fehler" : picture.uploadedBy.profilepicture}
+              width={35}
+            />
+            <p className="text-tiny uppercase font-bold">{picture.uploadedBy.username}</p>
+          </div>
+          <small className="text-default-500">Likes</small>
+          <small className="text-default-500">Views</small>
+        </CardFooter>
+      </Card>
+
+      <div className="pt-4 pb-4"></div>
+
+      <Card className="py-4">
+
+        <CardBody className="overflow-visible py-2">
+          <Image
+            alt="Card background"
+            className="object-cover"
             src={picture.img === null ? "Fehler" : URL.createObjectURL(picture.img)}
             width={270}
           />
         </CardBody>
+        <CardHeader className="pb-0 pt-0 px-4 flex-col items-start">
+          <h4 className="font-bold text-large">{picture.title}</h4>
+        </CardHeader>
+        <CardFooter className="cardFooter">
+          <div className="flex gap-2 items-center">
+            <Image
+              alt="User Profile"
+              className="object-cover none"
+              src={picture.img === null ? "Fehler" : picture.uploadedBy.profilepicture}
+              width={35}
+            />
+            <p className="text-tiny uppercase font-bold">{picture.uploadedBy.username}</p>
+          </div>
+          <small className="text-default-500">Likes</small>
+          <small className="text-default-500">Views</small>
+        </CardFooter>
       </Card>
 
 
