f9c4913f66a8df6ddc6b4cba4d2f1f7be33aca38
alexanderbenski <alexanderbenski@gmail.com>
2024-08-25 14:58:53 +0000

    ✨ adds page for QuoteUpload

---
+++ app/quotes/(detail)/upload/page.tsx
@@ -0,0 +1,14 @@
+import Navigation from "@/lib/components/Navigation/navigation";
+import QuoteUpload from "@/lib/components/quotes/quotes-upload/quote-upload";
+
+export default function Page() {
+  
+  // import data for list here
+
+  return (
+    <div>
+      <Navigation></Navigation>
+      <QuoteUpload></QuoteUpload>
+    </div>
+  )
+}
