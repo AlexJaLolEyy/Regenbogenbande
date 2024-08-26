e9478a06e71ad54774e6520769f4369453ffd793
alexanderbenski <alexanderbenski@gmail.com>
2024-08-26 20:20:10 +0000

    ♻️ refactor quote list and moved data import to page

--- lib/components/quotes/quotes-list/quote-list.tsx
+++ lib/components/quotes/quotes-list/quote-list.tsx
@@ -1,29 +1,20 @@
 'use client'
 
-import { useEffect, useState } from "react";
 import { Quote } from "../../../types/types"
-import { getExampleQuotes } from "@/app/current-storage/storage";
 import QuoteComponent from "../quote/quote";
 import Link from "next/link";
 
 import "./quote-list.css";
 
 // TODO: remove the "?" from quotes and supply data from page"
-export default function QuoteList({ quotes }: { quotes?: Quote[] }) {
-
-    const [fakeQuotes, setFakeQuotes] = useState<Quote[] | null>(null);
-
-    useEffect(() => {
-        // gets all quotes
-        getExampleQuotes().then((data) => setFakeQuotes(data));
-    }, []);
+export default function QuoteList({ quotes }: { quotes: Quote[] }) {
 
     return (
         <div>
             <Link href="/quotes/upload">Go to Upload</Link>
             <div className="quoteList">
-                {fakeQuotes != null ?
-                    fakeQuotes.map((quote) => (
+                {quotes != null && quotes != undefined ?
+                    quotes.map((quote) => (
                         <QuoteComponent quote={quote}></QuoteComponent>
                     )) : ""}
             </div>
