5aa8cf5a3c88fd330cbbe9b6b9281a26b1f2601c
Alex <alexanderbenski@gmail.com>
2024-09-01 23:52:37 +0200

    ✨ adds function to get, add, edit and delete items from local files

--- app/current-storage/storage.tsx
+++ app/current-storage/storage.tsx
@@ -1,7 +1,7 @@
 'use server'
 
 import { Picture, Quote, User, Video } from "@/lib/types/types";
-const fs = require('fs');
+import { promises as fs } from 'fs';
 
 const user1: User = {
     username: "Alex Ja Lol Eyy",
@@ -200,6 +200,7 @@ export async function getAllUsers(): Promise<User[]> {
 }
 
 
+// TODO: rework the getById's to adjust them to get data from the file system
 // info: id is type number but bc it gets delivered by url its still a string -> == instead of ===
 export async function getQuoteById(id: number): Promise<Quote> {
     return (await getExampleQuotes()).filter((quote) => quote.id == id)[0];
@@ -220,26 +221,146 @@ export async function getUserById(id: number): Promise<User> {
 }
 
 export async function addQuote(quote: Quote): Promise<void> {
-    // implement add to local file later
+    var allQuotes: Quote[] = await getAllQuotes();
+    const exists = allQuotes.some(qt => qt.id === quote.id)
+
+    if (exists) {
+        console.error("Quote already exists, not adding!");
+        return;
+    }
+    else {
+        allQuotes.push(quote);
+        await fs.writeFile('app/current-storage/data/pictures.json', JSON.stringify(allQuotes, null, 4));
+    }
 }
 
 export async function addPicture(picture: Picture): Promise<void> {
-    // implement add to local file later
+    var allPictures: Picture[] = await getAllPictures();
+    const exists = allPictures.some(pic => pic.id === picture.id)
+
+    if (exists) {
+        console.error("Picture already exists, not adding!");
+        return;
+    }
+    else {
+        allPictures.push(picture);
+        await fs.writeFile('app/current-storage/data/pictures.json', JSON.stringify(allPictures, null, 4));
+    }
 }
 
 export async function addVideo(video: Video): Promise<void> {
-    // implement add to local file later
-    await fs.writeFileSync('app\current-storage\storageData.json', JSON.stringify(video, null, 4));
+    var allVideos: Video[] = await getAllVideos();
+    const exists = allVideos.some(vid => vid.id === video.id)
+
+    if (exists) {
+        console.error("Video already exists, not adding!");
+        return;
+    }
+    else {
+        allVideos.push(video);
+        await fs.writeFile('app/current-storage/data/videos.json', JSON.stringify(allVideos, null, 4));
+    }
 }
 
 export async function deleteQuote(quoteId: number): Promise<void> {
-    // implement add to local file later
+    var allQuotes: Quote[] = await getAllQuotes();
+    const remaining: Quote[] = allQuotes.filter((quote) => quote.id !== quoteId)
+    await fs.writeFile('app/current-storage/data/pictures.json', JSON.stringify(remaining, null, 4));
 }
 
 export async function deletePicture(pictureId: number): Promise<void> {
-    // implement add to local file later
+    var allPictures: Picture[] = await getAllPictures();
+    const remaining: Picture[] = allPictures.filter((picture) => picture.id !== pictureId)
+    await fs.writeFile('app/current-storage/data/pictures.json', JSON.stringify(remaining, null, 4));
 }
 
+// TODO: technically theres still a case when videoId is invalid (but if thats the case... rip)
 export async function deleteVideo(videoId: number): Promise<void> {
-    // implement add to local file later
+    var allVideos: Video[] = await getAllVideos();
+    const remaining: Video[] = allVideos.filter((video) => video.id !== videoId)
+    await fs.writeFile('app/current-storage/data/videos.json', JSON.stringify(remaining, null, 4));
+}
+
+export async function getAllVideos(): Promise<Video[]> {
+    return JSON.parse(await fs.readFile('app/current-storage/data/videos.json', "utf-8"));
+}
+
+export async function getAllPictures(): Promise<Picture[]> {
+    return JSON.parse(await fs.readFile('app/current-storage/data/pictures.json', "utf-8"));
+}
+
+export async function getAllQuotes(): Promise<Quote[]> {
+    return JSON.parse(await fs.readFile('app/current-storage/data/quotes.json', "utf-8"));
+}
+
+export async function getAllUsers(): Promise<User[]> {
+    return JSON.parse(await fs.readFile('app/current-storage/data/users.json', "utf-8"));
+}
+
+export async function addUser(user: User): Promise<void> {
+    var userlist: User[] = await getAllUsers();
+    const exists = userlist.some(us => us.id === user.id)
+
+    if (exists) {
+        console.error("User already exists, not adding!");
+        return;
+    }
+    else {
+        userlist.push(user);
+        await fs.writeFile('app/current-storage/data/users.json', JSON.stringify(userlist, null, 4));
+    }
+}
+
+export async function deleteUser(userid: number): Promise<void> {
+    var userlist: User[] = await getAllUsers();
+    const remaining: User[] = userlist.filter((user) => user.id !== userid)
+    await fs.writeFile('app/current-storage/data/users.json', JSON.stringify(remaining, null, 4));
+}
+
+// use the userlist.map as input for the writeFile (might cause readability trouble)
+export async function editUser(updatedUser: User): Promise<void> {
+    var userlist: User[] = await getAllUsers();
+    userlist = userlist.map((user) => {
+        if (user.id === updatedUser.id) {
+            return updatedUser;
+        }
+        return user;
+    });
+    await fs.writeFile('app/current-storage/data/users.json', JSON.stringify(userlist, null, 4));
+}
+
+// use the .map as input for the writeFile (might cause readability trouble)
+export async function editVideo(updatedVideo: Video): Promise<void> {
+    var allVideos: Video[] = await getAllVideos();
+    allVideos = allVideos.map((video) => {
+        if (video.id === updatedVideo.id) {
+            return updatedVideo;
+        }
+        return video;
+    });
+    await fs.writeFile('app/current-storage/data/videos.json', JSON.stringify(allVideos, null, 4));
+}
+
+// use the .map as input for the writeFile (might cause readability trouble)
+export async function editPicture(updatedPicture: Picture): Promise<void> {
+    var allPictures: Picture[] = await getAllPictures();
+    allPictures = allPictures.map((picture) => {
+        if (picture.id === updatedPicture.id) {
+            return updatedPicture;
+        }
+        return picture;
+    });
+    await fs.writeFile('app/current-storage/data/pictures.json', JSON.stringify(allPictures, null, 4));
+}
+
+// use the .map as input for the writeFile (might cause readability trouble)
+export async function editQuote(updatedQuote: Quote): Promise<void> {
+    var allQuotes: Quote[] = await getAllQuotes();
+    allQuotes = allQuotes.map((quote) => {
+        if (quote.id === updatedQuote.id) {
+            return updatedQuote;
+        }
+        return quote;
+    });
+    await fs.writeFile('app/current-storage/data/quotes.json', JSON.stringify(allQuotes, null, 4));
 }
