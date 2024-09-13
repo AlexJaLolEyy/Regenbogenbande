e7a6b2491a1bdd652cdfd73e8c162e68c47c8e89
Alex <alexanderbenski@gmail.com>
2024-09-13 23:36:34 +0200

    ✨ updated storage to work with data in json files

--- app/current-storage/storage.tsx
+++ app/current-storage/storage.tsx
@@ -3,217 +3,219 @@
 import { Picture, Quote, User, Video } from "@/lib/types/types";
 import { promises as fs } from 'fs';
 
-const user1: User = {
-    username: "Alex Ja Lol Eyy",
-    password: "12345",
-    profilepicture: "https://nextui.org/images/album-cover.png",
-    id: 1
-}
+// const user1: User = {
+//     username: "Alex Ja Lol Eyy",
+//     password: "12345",
+//     profilepicture: "https://nextui.org/images/album-cover.png",
+//     id: 1
+// }
 
-const user2: User = {
-    username: "Thylon",
-    password: "12345",
-    profilepicture: "https://nextui.org/images/album-cover.png",
-    id: 2
-}
+// const user2: User = {
+//     username: "Thylon",
+//     password: "12345",
+//     profilepicture: "https://nextui.org/images/album-cover.png",
+//     id: 2
+// }
 
 
-export async function getExampleVideos(): Promise<Video[]> {
+// export async function getExampleVideos(): Promise<Video[]> {
 
-    const video1: Video = {
-        title: "beispiel 1",
-        description: "beispiel video 1",
-        video: "/exampleVideos/beispielV1.mp4",
-        thumbnail: "/examplePictures/exampleThumbnail.jpg",
-        id: 1,
-        participants: [user1, user2],
-        uploadedBy: user1,
-        uploadedAt: new Date("2023-06-01"),
-        createdAt: new Date("2023-06-01")
-    }
+//     const video1: Video = {
+//         title: "beispiel 1",
+//         description: "beispiel video 1",
+//         video: "/exampleVideos/beispielV1.mp4",
+//         thumbnail: "/examplePictures/exampleThumbnail.jpg",
+//         id: 1,
+//         participants: [user1, user2],
+//         uploadedBy: user1,
+//         uploadedAt: new Date("2023-06-01"),
+//         createdAt: new Date("2023-06-01")
+//     }
 
-    const video2: Video = {
-        title: "beispiel 2",
-        description: "beispiel video 2",
-        video: "/exampleVideos/beispielV2.mp4",
-        thumbnail: "/examplePictures/exampleThumbnail.jpg",
-        id: 2,
-        participants: [user1],
-        uploadedBy: user1,
-        uploadedAt: new Date("2023-06-01"),
-        createdAt: new Date("2023-06-01")
-    }
+//     const video2: Video = {
+//         title: "beispiel 2",
+//         description: "beispiel video 2",
+//         video: "/exampleVideos/beispielV2.mp4",
+//         thumbnail: "/examplePictures/exampleThumbnail.jpg",
+//         id: 2,
+//         participants: [user1],
+//         uploadedBy: user1,
+//         uploadedAt: new Date("2023-06-01"),
+//         createdAt: new Date("2023-06-01")
+//     }
 
-    var videos: Video[] = [video1, video2];
+//     var videos: Video[] = [video1, video2];
 
-    return JSON.parse(JSON.stringify(videos));
-}
+//     return JSON.parse(JSON.stringify(videos));
+// }
 
 
 // TODO: optimize return and adjust test data
-export async function getExamplePictures(): Promise<Picture[]> {
+// export async function getExamplePictures(): Promise<Picture[]> {
 
-    const picture1: Picture = {
-        title: "blue",
-        description: "blue picture",
-        img: "/examplePictures/pictureColorBlue.png",
-        id: 22,
-        participants: [user1],
-        uploadedBy: user1,
-        uploadedAt: new Date("2023-06-01"),
-        createdAt: new Date("2023-06-01")
-    }
+//     const picture1: Picture = {
+//         title: "blue",
+//         description: "blue picture",
+//         img: "/examplePictures/pictureColorBlue.png",
+//         id: 22,
+//         participants: [user1],
+//         uploadedBy: user1,
+//         uploadedAt: new Date("2023-06-01"),
+//         createdAt: new Date("2023-06-01")
+//     }
 
-    const picture2: Picture = {
-        title: "greenBlue",
-        description: "greenBlue picture",
-        img: "/examplePictures/pictureColorGreenBlue.png",
-        id: 23,
-        participants: [user1],
-        uploadedBy: user1,
-        uploadedAt: new Date("2023-06-01"),
-        createdAt: new Date("2023-06-01")
-    }
-    const picture3: Picture = {
-        title: "lightGreen",
-        description: "lightGreen picture",
-        img: "/examplePictures/pictureColorLightGreen.png",
-        id: 24,
-        participants: [user1],
-        uploadedBy: user1,
-        uploadedAt: new Date("2023-06-01"),
-        createdAt: new Date("2023-06-01")
-    }
-    const picture4: Picture = {
-        title: "purple",
-        description: "purple picture",
-        img: "/examplePictures/pictureColorPurple.png",
-        id: 25,
-        participants: [user1],
-        uploadedBy: user1,
-        uploadedAt: new Date("2023-06-01"),
-        createdAt: new Date("2023-06-01")
-    }
-    var pictures: Picture[] = [picture1, picture2, picture3, picture4];
+//     const picture2: Picture = {
+//         title: "greenBlue",
+//         description: "greenBlue picture",
+//         img: "/examplePictures/pictureColorGreenBlue.png",
+//         id: 23,
+//         participants: [user1],
+//         uploadedBy: user1,
+//         uploadedAt: new Date("2023-06-01"),
+//         createdAt: new Date("2023-06-01")
+//     }
+//     const picture3: Picture = {
+//         title: "lightGreen",
+//         description: "lightGreen picture",
+//         img: "/examplePictures/pictureColorLightGreen.png",
+//         id: 24,
+//         participants: [user1],
+//         uploadedBy: user1,
+//         uploadedAt: new Date("2023-06-01"),
+//         createdAt: new Date("2023-06-01")
+//     }
+//     const picture4: Picture = {
+//         title: "purple",
+//         description: "purple picture",
+//         img: "/examplePictures/pictureColorPurple.png",
+//         id: 25,
+//         participants: [user1],
+//         uploadedBy: user1,
+//         uploadedAt: new Date("2023-06-01"),
+//         createdAt: new Date("2023-06-01")
+//     }
+//     var pictures: Picture[] = [picture1, picture2, picture3, picture4];
 
-    return JSON.parse(JSON.stringify(pictures));
-}
+//     var test = [picture1, picture2, picture3, picture4];
+//     console.log(JSON.stringify(test))
+
+//     return JSON.parse(JSON.stringify(pictures));
+// }
 
 // TODO: optimize return and adjust test data
-export async function getExampleQuotes(): Promise<Quote[]> {
+// export async function getExampleQuotes(): Promise<Quote[]> {
 
-    const quote1: Quote = {
-        fullQuote: [{
-            msg: "Hello There!",
-            user: user1
-        },
-        {
-            msg: "General Kenobi!",
-            user: user2
-        }],
-        id: 1,
-        participants: [user1, user2],
-        uploadedBy: user1,
-        uploadedAt: new Date("2023-06-01"),
-        createdAt: new Date("2023-06-01")
-    }
+//     const quote1: Quote = {
+//         fullQuote: [{
+//             msg: "Hello There!",
+//             user: user1
+//         },
+//         {
+//             msg: "General Kenobi!",
+//             user: user2
+//         }],
+//         id: 1,
+//         participants: [user1, user2],
+//         uploadedBy: user1,
+//         uploadedAt: new Date("2023-06-01"),
+//         createdAt: new Date("2023-06-01")
+//     }
 
-    const quote2: Quote = {
-        fullQuote: [{
-            msg: "Test1234",
-            user: user1
-        },
-        {
-            msg: "Check!",
-            user: user2
-        }],
-        id: 2,
-        participants: [user1, user2],
-        uploadedBy: user2,
-        uploadedAt: new Date("2023-06-02"),
-        createdAt: new Date("2023-06-02")
-    }
+//     const quote2: Quote = {
+//         fullQuote: [{
+//             msg: "Test1234",
+//             user: user1
+//         },
+//         {
+//             msg: "Check!",
+//             user: user2
+//         }],
+//         id: 2,
+//         participants: [user1, user2],
+//         uploadedBy: user2,
+//         uploadedAt: new Date("2023-06-02"),
+//         createdAt: new Date("2023-06-02")
+//     }
 
-    const quote3: Quote = {
-        fullQuote: [{
-            msg: "Hallo wie gehts dir?",
-            user: user1
-        },
-        {
-            msg: "Mir geht es gut und wie geht es dir?",
-            user: user2
-        },
-        {
-            msg: "Same here, danke der Nachfrage!",
-            user: user1
-        }],
-        id: 3,
-        participants: [user1, user2],
-        uploadedBy: user1,
-        uploadedAt: new Date("2023-06-05"),
-        createdAt: new Date("2023-06-05")
-    }
+//     const quote3: Quote = {
+//         fullQuote: [{
+//             msg: "Hallo wie gehts dir?",
+//             user: user1
+//         },
+//         {
+//             msg: "Mir geht es gut und wie geht es dir?",
+//             user: user2
+//         },
+//         {
+//             msg: "Same here, danke der Nachfrage!",
+//             user: user1
+//         }],
+//         id: 3,
+//         participants: [user1, user2],
+//         uploadedBy: user1,
+//         uploadedAt: new Date("2023-06-05"),
+//         createdAt: new Date("2023-06-05")
+//     }   
+    
+//     return [quote1, quote2, quote3];
+// }
 
-    return [quote1, quote2, quote3];
-}
-
-export async function getAllUsers(): Promise<User[]> {
-    var users: User[] = [
-        {
-            username: "Alex Ja Lol Eyy",
-            password: "12345",
-            profilepicture: "https://nextui.org/images/album-cover.png",
-            id: 1
-        },
-        {
-            username: "Thylon",
-            password: "12345",
-            profilepicture: "https://nextui.org/images/album-cover.png",
-            id: 2
-        },
-        {
-            username: "T1 Sensei",
-            password: "12345",
-            profilepicture: "https://nextui.org/images/album-cover.png",
-            id: 3
-        },
-        {
-            username: "Jesse",
-            password: "12345",
-            profilepicture: "https://nextui.org/images/album-cover.png",
-            id: 4
-        },
-        {
-            username: "Achel aka BigBrainGamer420",
-            password: "12345",
-            profilepicture: "https://nextui.org/images/album-cover.png",
-            id: 5
-        },
-        {
-            username: "Marla",
-            password: "12345",
-            profilepicture: "https://nextui.org/images/album-cover.png",
-            id: 6
-        }
-    ];
-    return users;
-}
+// export async function getAllUsers(): Promise<User[]> {
+//     var users: User[] = [
+//         {
+//             username: "Alex Ja Lol Eyy",
+//             password: "12345",
+//             profilepicture: "https://nextui.org/images/album-cover.png",
+//             id: 1
+//         },
+//         {
+//             username: "Thylon",
+//             password: "12345",
+//             profilepicture: "https://nextui.org/images/album-cover.png",
+//             id: 2
+//         },
+//         {
+//             username: "T1 Sensei",
+//             password: "12345",
+//             profilepicture: "https://nextui.org/images/album-cover.png",
+//             id: 3
+//         },
+//         {
+//             username: "Jesse",
+//             password: "12345",
+//             profilepicture: "https://nextui.org/images/album-cover.png",
+//             id: 4
+//         },
+//         {
+//             username: "Achel aka BigBrainGamer420",
+//             password: "12345",
+//             profilepicture: "https://nextui.org/images/album-cover.png",
+//             id: 5
+//         },
+//         {
+//             username: "Marla",
+//             password: "12345",
+//             profilepicture: "https://nextui.org/images/album-cover.png",
+//             id: 6
+//         }
+//     ];
+//     return users;
+// }
 
 
-// TODO: rework the getById's to adjust them to get data from the file system
 // info: id is type number but bc it gets delivered by url its still a string -> == instead of ===
 export async function getQuoteById(id: number): Promise<Quote> {
-    return (await getExampleQuotes()).filter((quote) => quote.id == id)[0];
+    return (await getAllQuotes()).filter((quote) => quote.id == id)[0];
 }
 
 // info: id is type number but bc it gets delivered by url its still a string -> == instead of ===
 export async function getPictureById(id: number): Promise<Picture> {
-    return (await getExamplePictures()).filter((picture) => picture.id == id)[0];
+    return (await getAllPictures()).filter((picture) => picture.id == id)[0];
 }
 
 // info: id is type number but bc it gets delivered by url its still a string -> == instead of ===
 export async function getVideoById(id: number): Promise<Video> {
-    return (await getExampleVideos()).filter((video) => video.id == id)[0];
+    return (await getAllVideos()).filter((video) => video.id == id)[0];
 }
 // info: id is type number but bc it gets delivered by url its still a string -> == instead of ===
 export async function getUserById(id: number): Promise<User> {
