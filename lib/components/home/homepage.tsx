af5323726b2b498525c2455ca41b8707792d5656
Alex <alexanderbenski@gmail.com>
2024-09-14 23:37:10 +0200

    ✨💄🚧 adds homepage component with placeholder background animation

---
+++ lib/components/home/homepage.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import "./homepage.scss";
+
+// TODO: currently only experimental, find new background
+export default function Homepage({} : {}
+) {
+
+    return (
+        <div className="homepage">
+            <p>You are currently at the Homepage!</p>
+
+            <h1>This is the Homepage of <ul>Die Regenbogenbande</ul></h1>
+            <div className="backgroud">
+                <div className="stars">
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
