b0742be9aff3cf6074ef6352e6bf708ec0a6ca49
Alex <alexanderbenski@gmail.com>
2024-09-16 21:16:01 +0200

    💄🚧 adds first version of UI for video-view

--- lib/components/videos/video-view/video-view.tsx
+++ lib/components/videos/video-view/video-view.tsx
@@ -4,6 +4,10 @@ import { Video } from "../../../types/types"
 import Link from "next/link";
 
 import { useEffect } from "react";
+import { Breadcrumbs, BreadcrumbItem, User, Textarea } from "@nextui-org/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
+import "./video-view.scss";
 
 export default function VideoView({ video }: { video: Video }
 ) {
@@ -14,19 +18,67 @@ export default function VideoView({ video }: { video: Video }
 
   return (
     <div>
-      <p>You are currently at Video-View!</p>
 
-      <Link href="/pictures/upload">Go to Upload</Link>
+      <Breadcrumbs>
+        <BreadcrumbItem href="/">Home</BreadcrumbItem>
+        <BreadcrumbItem href="/videos">Videos</BreadcrumbItem>
+        <BreadcrumbItem href="">Video-View</BreadcrumbItem>
+      </Breadcrumbs>
+
+      <Link href="/videos/upload">Go to Upload</Link>
+
+
+      <div className="title">
+        <h1>{video.title}</h1>
+      </div>
+      {/* <button onClick={() => addVideo(video)}>Try Adding</button> */}
 
       <video width="1280" height="720" controls>
         <source src={video.video} type="video/mp4"></source>
         Video cant be displayed due to error...
       </video>
 
-      <p>Title: {video.title}</p>
-      <p>Description: {video.description}</p>
-      <p>ID: {video.id}</p>
-      <p>UploadedBy: {JSON.stringify(video.uploadedBy)}</p>
+      <div className="metadata">
+        <div className="uploadedBy">
+          <label>Uploaded by</label>
+          <User
+            name={video.uploadedBy.username}
+            avatarProps={{
+              src: video.uploadedBy.profilepicture
+            }}
+          />
+        </div>
+        <div className="uploadedAt">
+          <label>Uploaded at</label>
+          <p>{video.uploadedAt.toLocaleDateString()}</p>
+        </div>
+        <div className="createdAt">
+          <label>Created at</label>
+          <p>{video.createdAt.toLocaleDateString()}</p>
+        </div>
+        <div className="rating">
+          <label>Rating</label>
+          <div className="stars">
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+          </div>
+        </div>
+      </div>
+
+      <div className="description">
+        <Textarea
+          isReadOnly
+          label="Description"
+          variant="bordered"
+          labelPlacement="inside"
+          placeholder="Enter your description"
+          defaultValue={video.description}
+          className="max-w"
+        />
+      </div>
 
     </div>
   );
