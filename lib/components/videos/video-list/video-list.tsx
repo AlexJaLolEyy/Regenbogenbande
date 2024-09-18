f691db6438c818bfaf2dabecf28a0f56f05ebf37
Alex <alexanderbenski@gmail.com>
2024-09-18 02:09:33 +0200

    💄🚧 updated UI for video-list

--- lib/components/videos/video-list/video-list.tsx
+++ lib/components/videos/video-list/video-list.tsx
@@ -1,20 +1,23 @@
 "use client";
 
+import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
+import Link from "next/link";
 import type { Video } from "../../../types/types";
 import VideoComponent from "../video/video";
-import Link from "next/link";
-
 import "./video-list.css";
 
-
-// TODO: adjust whole component -> change pics to vids
-// TODO: remove the "?" from videos and supply data from page
 export default function VideoList({ videos }: { videos: Video[] }) {
 
   return (
     <div>
-      <p>You are currently at Video-List!</p>
-      <Link href="/videos/upload">Go to Upload</Link>
+      
+      <Breadcrumbs>
+        <BreadcrumbItem href="/">Home</BreadcrumbItem>
+        <BreadcrumbItem href="/videos">Videos</BreadcrumbItem>
+      </Breadcrumbs>
+
+      <h1>Select a Video!</h1>
+      <h2><Link href="/videos/upload">Go to Upload</Link></h2>
 
       <div className="videoList">
         {videos != null || videos != undefined ?
