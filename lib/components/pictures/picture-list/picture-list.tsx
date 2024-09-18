d23a03468148670e57124aa0c902c12dfe58d9b9
Alex <alexanderbenski@gmail.com>
2024-09-18 02:07:13 +0200

    💄🚧 updated UI for picture-list

--- lib/components/pictures/picture-list/picture-list.tsx
+++ lib/components/pictures/picture-list/picture-list.tsx
@@ -1,30 +1,35 @@
 "use client"
 
-import { Picture } from "../../../types/types"
+import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
 import Link from "next/link";
+import { Picture } from "../../../types/types";
 import PictureComponent from "../picture/picture";
-
 import "./picture-list.css";
 
-// TODO: remove the "?" from pictures and supply data from page"
 export default function PictureList({ pictures }: { pictures: Picture[] }
 ) {
-  
-    return (
-      <div>
-        <p>You are currently at Picture-List!</p>
-        <Link href="/pictures/upload">Go to Upload</Link>
 
-        <div className="pictureList">
-          {pictures != null && pictures != undefined
-            ? pictures.map((pic) => (
-              <div key={pic.id}>
-                <PictureComponent picture={pic}></PictureComponent>
-              </div>
-            ))
-            : "error while loading pictures"}
-        </div>
+  return (
+    <div>
 
+      <Breadcrumbs>
+        <BreadcrumbItem href="/">Home</BreadcrumbItem>
+        <BreadcrumbItem href="/pictures">Pictures</BreadcrumbItem>
+      </Breadcrumbs>
+
+      <h1>Select a Picture!</h1>
+      <h2><Link href="/pictures/upload">Go to Upload</Link></h2>
+
+      <div className="pictureList">
+        {pictures != null && pictures != undefined
+          ? pictures.map((pic) => (
+            <div key={pic.id}>
+              <PictureComponent picture={pic}></PictureComponent>
+            </div>
+          ))
+          : "error while loading pictures"}
       </div>
-    );
+
+    </div>
+  );
 }
