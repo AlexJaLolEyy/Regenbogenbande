e913933c384e87f68d8689828934b05ee911d1ca
Alex <alexanderbenski@gmail.com>
2024-09-18 02:06:21 +0200

    💄🚧 updated UI for quote-list

--- lib/components/quotes/quotes-list/quote-list.tsx
+++ lib/components/quotes/quotes-list/quote-list.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { Quote } from "../../../types/types"
-import QuoteComponent from "../quote/quote";
+import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
 import Link from "next/link";
-
+import { Quote } from "../../../types/types";
+import QuoteComponent from "../quote/quote";
 import "./quote-list.css";
 
 // TODO: remove the "?" from quotes and supply data from page"
@@ -11,11 +11,21 @@ export default function QuoteList({ quotes }: { quotes: Quote[] }) {
 
     return (
         <div>
-            <Link href="/quotes/upload">Go to Upload</Link>
+
+            <Breadcrumbs>
+                <BreadcrumbItem href="/">Home</BreadcrumbItem>
+                <BreadcrumbItem href="/quotes">Quotes</BreadcrumbItem>
+            </Breadcrumbs>
+
+            <h1>Select a Video!</h1>
+            <h2><Link href="/quotes/upload">Go to Upload</Link></h2>
+
             <div className="quoteList">
                 {quotes != null && quotes != undefined ?
                     quotes.map((quote) => (
-                        <QuoteComponent quote={quote}></QuoteComponent>
+                        <div key={quote.id}>
+                            <QuoteComponent quote={quote}></QuoteComponent>
+                        </div>
                     )) : ""}
             </div>
         </div>
