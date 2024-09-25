b7f5324534f84a48c32d4bd3de6e63b0c23ce3cf
Alex <alexanderbenski@gmail.com>
2024-09-25 21:18:22 +0200

    🐛 fixed hydration error due to having multiple html and body tags

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -5,33 +5,20 @@ import '../globals.css'
 import Navigation from '@/lib/components/Navigation/navigation';
 import { Providers } from '../providers';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Regenbogenbande',
-  description: 'Website featuring content for the "Regenbogenbande"',
-}
-
-export default function RootLayout({
+export default function ContentLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-
-    <html lang="en" className='light'>
-      <body className={inter.className}>
-      <Navigation></Navigation>
-        <div className='layout'>
-          <div className='left-box'></div>
-          <div className='content-box'>
-            <Providers>
-              {children}
-            </Providers>
-          </div>
-          <div className='right-box'></div>
-        </div>
-      </body>
-    </html>
+    <div className="layout">
+      <div className="left-box"></div>
+      <div className="content-box">
+        <Providers>
+          {children}
+        </Providers>
+      </div>
+      <div className="right-box"></div>
+    </div>
   )
 }
