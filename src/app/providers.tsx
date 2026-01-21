b1b7342202aff9860c0c6b61e5a22aeaa59b86e9
Alex <alexanderbenski@gmail.com>
2026-01-21 03:21:52 +0100

    ✨ feature: add TanStackQuery-Provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
+import { QueryProvider } from "../lib/components/providers/query-client-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <HeroUIProvider>
-      {children}
-    </HeroUIProvider>
+    <QueryProvider>
+      <HeroUIProvider>
+        {children}
+      </HeroUIProvider>
+    </QueryProvider>
   )
 }
